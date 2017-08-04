03bc43a6b5ace3fc68a968a89d5337073c8570f1
jluukvg <j.luukvg@gmail.com>
2017-08-04 02:58:59 -0500

    Save unfinished changes

---
+++ app/src/main/java/com/jluukvg/uclassify/data/network/ApiEndPoint.java
@@ -0,0 +1,12 @@
+package com.jluukvg.uclassify.data.network;
+
+/**
+ * Created by jluukvg on 8/3/17.
+ */
+
+public class ApiEndPoint {
+
+    public static final String BASE_URL = "https://api.uclassify.com/v1/";
+
+
+}
--- app/src/main/java/com/jluukvg/uclassify/data/network/uClassifyApi.java
+++ app/src/main/java/com/jluukvg/uclassify/data/network/uClassifyApi.java
@@ -1,5 +1,15 @@
 package com.jluukvg.uclassify.data.network;
 
+import com.jluukvg.uclassify.data.network.model.ClassifierResponse;
+
+import retrofit2.Call;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
 public interface uClassifyApi {
+
+    @POST("{username}/{classifierName}/classify")
+    Call<ClassifierResponse> getTopicProbabilities(@Path("username") String username,
+                                                   @Path("classifierName") String classifierName);
 
 }
